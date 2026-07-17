6057e0faef49905f046ed7b6107cb73e67c4925c
priyan-18 <priyadharshandharshan9@gmail.com>
2026-07-17 08:53:55 +0530

    Demo: Add command execution endpoint

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const { exec } = require("child_process");
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello DevSecOps!");
+});
+
+app.get("/run", (req, res) => {
+    const cmd = req.query.cmd;
+
+    exec(cmd, (err, stdout, stderr) => {
+        if (err) {
+            return res.status(500).send(stderr);
+        }
+
+        res.send(stdout);
+    });
 });
 
 app.listen(3000, () => {
